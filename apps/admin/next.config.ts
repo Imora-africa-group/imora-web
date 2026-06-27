c9d8a499faf833710eeef1b8e90d34ed31f68af3
KOUGBLENOU Sebastien Donan <sebastien.kougblenou@epitech.eu>
2026-06-27 10:03:58 +0100

    birnary

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -6,6 +6,14 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 const nextConfig: NextConfig = {
   transpilePackages: ['@imora/ui', '@imora/db', '@imora/types'],
   serverExternalPackages: ['cloudinary', '@prisma/client', 'bcryptjs'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '**': [
+        '../../node_modules/.pnpm/@prisma+client@*/node_modules/.prisma/client/*.node',
+        '../../node_modules/.prisma/client/*.node',
+      ],
+    },
+  },
   images: {
     remotePatterns: [
       {
